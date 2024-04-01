c38462534e1f65652817f7f32f4dbe8d45c2cfdf
Jessika Solsona De Souza Wande <ajessika@minsait.com>
2024-03-31 21:23:46 -0300

    Aula 27_03_24 POO e Encapsulamento

--- src/br/com/ajssw/Principal.java
+++ src/br/com/ajssw/Principal.java
@@ -1,62 +1,61 @@
 package br.com.ajssw;
 
-public class Principal {
+import br.com.ajssw.model.Carro;
 
+public class Principal {
+	
 	public static void main(String[] args) {
-		System.out.println("Iniciando com o Java");
+		// TODO Auto-generated method stub
+		System.out.println("Iniciando com o Java");		
+		System.out.println("--------------------");
 		
-		System.out.println("----------------------");
-		
+		/*
 		boolean condicao = true;
-		if(condicao == true) {
+		if(!condicao) { 
 			System.out.println("Condição verdadeira!");
-		}else {
+		} else { 
 			System.out.println("Condição falsa");
 		}
 		System.out.println("--------------------");
 		
-		//Validar nota do aluno
-		
+		// validar nota do aluno
 		int nota = 2;
 		if(nota >= 6) {
 			System.out.println("Passou de ano!");
 		} else if(nota <= 3) {
 			System.out.println("Reprovou!");
 		} else {
-			System.out.println("Encontra-se em recuperação!");
-			
+			System.out.println("Recuperação....");
 		}
 		
 		
 		switch(nota) {
-				case 5:
-					System.out.println("Recuperação");
-					break;
-				case 3:
-					System.out.println("Reprovou");
-					break;
-				case 6:
-				case 7:
-				case 8:
-				case 9:
-				case 10:
-					System.out.println("Passou!");
-					break;
-				default:
-					System.out.println("Limbo");
-					break;
+			case 5:
+				System.out.println("Recuperação");
+				break;
+			case 3:
+				System.out.println("Reprovou");
+				break;
+			case 6:
+			case 7:
+			case 8:
+			case 9:
+			case 10:
+				System.out.println("Passou!");
+				break;
+			default:
+				System.out.println("Limbo");
+				break;
 		}
 		System.out.println("--------------------");
-	
+		
 		// estruturas de repetição
 		// for; while
 		for(int x = 0; x <= 10; x++) {
 			System.out.print(x + ", ");
-			
 		}
-		
 		System.out.println("--------------------");
-	
+		
 		int z = 0;
 		while(z <= 200) {
 			System.out.print(z);
@@ -64,6 +63,19 @@ public class Principal {
 				System.out.print(", ");
 			z++;
 		}
+		System.out.println("--------------------");
+		*/
+		Carro carroJao = new Carro("Azul","BYD","PLUS"); //instanciei a classe no objeto		
+		Carro carroTiao = new Carro("Prata","BYD","PLUS","Automático"); //instanciei a classe no objeto
+		
+		System.out.println("Jao: " + carroJao.toString());
+		System.out.println("Tiao: " + carroTiao.toString());
+		
+		carroJao.setCor("Vermelho");
+		System.out.println("Jao: " + carroJao.toString());
+		
+		Carro carroMaria = new Carro("Branco", "TESLA","PLUS","AUTOMATICO","2.0");
+		System.out.println("Maria: " + carroMaria.toString());
 		
 	}
 
