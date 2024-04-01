d0ece092a24e60c3a8c0a23d6389f6bd88c6be1c
Jessika Solsona De Souza Wande <ajessika@minsait.com>
2024-03-31 21:39:21 -0300

    Aula 28_03_24 Herança e Inicio de Tratamento de Erros

---
+++ src/br/com/ajssw/model/Funcionario.java
@@ -0,0 +1,29 @@
+package br.com.ajssw.model;
+
+public class Funcionario extends Pessoa {
+	
+	private Double salario;
+	
+	public Double getSalario() {
+		return this.salario;
+	}
+	
+	public Funcionario(Double salario, String nome, String telefone, String email) {
+		super(nome, email, telefone);
+		this.salario = salario;
+	}
+	
+	public Funcionario(Double salario, String nome, String telefone) {
+		super(nome, telefone);
+		this.salario = salario;
+	}
+	
+	@Override
+	public String toString() {
+		return "Nome: "      + super.getNome()     + "\n" + 
+	           "Telefone: "  + super.getTelefone() + "\n" + 
+			   "E-mail: "    + super.getEmail()    + "\n" + 
+	           "Salario: R$" + this.salario; 
+	}
+
+}
